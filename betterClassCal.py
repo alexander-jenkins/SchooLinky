e1b5ef3381bae6c37c6b9bb7357c00ada4c31502
Alexander <22840490+alexander-jenkins@users.noreply.github.com>
2020-11-15 05:38:35 -0500

    almost at the mvp -- sorts the db based on whether a class is happening, has happened, or will happen. Next: make pretty

--- betterClassCal.py
+++ betterClassCal.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template, url_for, request, redirect, g
 from flask_sqlalchemy import SQLAlchemy
 import datetime
-import time
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Classes.db'
